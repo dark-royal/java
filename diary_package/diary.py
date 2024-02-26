5002b7b58d124e32e077432209b045cf8eeb9004
dark-royal <praiseoyewole560@gmail.com>
2024-02-26 14:59:26 +0100

    python

--- diary_package/diary.py
+++ diary_package/diary.py
@@ -1,10 +1,60 @@
+from diary_package.entry import Entry
+
+
+class Invalid_password_exception(BaseException):
+    pass
+
+
+def generate_id(entry_id):
+    entry_id += 1
+
+
 class Diary:
-    def __init__(self,user_name:str,password:str,entries:list):
+    def __init__(self, user_name: str, password: str):
         self.user_name = user_name
         self.password = password
-        #self.id = id
-        self
+        self.entries = []
+        self.is_locked = True
 
+    def get_user_name(self):
+        return self.user_name
 
+    def is_locked(self):
+        return self.is_locked
 
+    def unlock(self, password):
+        self.validate_password(password)
+        is_locked = False
 
+    @staticmethod
+    def lock_diary():
+        is_locked = True
+
+    def validate_password(self, password):
+        if self.password is not password:
+            raise Invalid_password_exception("incorrect password")
+        return False
+
+    def create_entry(self, title: str, body: str, entry_id):
+        generate_id(entry_id)
+        entry = Entry(entry_id, title, body)
+        self.entries.append(entry)
+        return entry
+
+    def delete_entry(self, entry_id):
+        entry = self.find_entry_by_id(entry_id)
+        self.entries.remove(entry_id)
+
+    def get_number_of_entry(self):
+        return self.entries
+
+    def find_entry_by_id(self, entry_id):
+        for entry in self.entries:
+            if entry.entry_id == entry_id:
+                return entry_id
+
+    def update_entry(self, entry_id: int, title: str, body: str):
+        for entry in self.entries:
+            if entry.get_id() == entry_id:
+                entry._title = title
+                entry._body = body
--- diary_package/entry.py
+++ diary_package/entry.py
@@ -20,7 +20,6 @@ class Entry:
     def date_time_created(self):
         return self.date_time_created
 
-
     @property
     def body(self):
         return self.body
@@ -30,19 +29,19 @@ class Entry:
         self._entry_id = value
 
     @title.setter
-    def title(self,title):
+    def title(self, title):
         self._title = title
 
     @body.setter
-    def body(self,body):
+    def body(self, body):
         self._body = body
 
     def __repr__(self):
         return f'entry id:{self._entry_id}\nbody: {self._title}\n' \
                f'Nin: {self._body}'
 
-
-
-
+    @date_time_created.setter
+    def date_time_created(self, value):
+        self._date_time_created = value
 
 
