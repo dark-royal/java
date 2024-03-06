7417fea384a94faaf3e0d0c92c3e06f73a2fc56c
dark-royal <praiseoyewole560@gmail.com>
2024-03-06 17:59:47 +0100

    python

--- python_sweet/Gun.py
+++ python_sweet/Gun.py
@@ -1,27 +1,32 @@
 from python_sweet.bullet_overloaded_exception import BulletOverLoadedException
+from python_sweet.invalid_pin_exception import InvalidPinException
 from python_sweet.no_bullet_found_exception import NoBulletFoundException
 
 
 class Gun:
-    def __init__(self):
+    def __init__(self,pin):
         self.num_of_bullet = 0
+        self.pin = pin
 
     def get_chamber(self):
         return self.num_of_bullet
 
-    def add_bullet(self):
+    def add_bullet(self,pin):
+        self.validate_pin(pin)
         if 5 > self.num_of_bullet >= 0:
             self.num_of_bullet += 1
         else:
             raise BulletOverLoadedException("Bullet should be 5")
 
-    def load_bullet(self):
+    def load_bullet(self,pin):
+        self.validate_pin(pin)
         if 5 > self.num_of_bullet >= 0:
             self.num_of_bullet += 1
             return self.num_of_bullet
         raise BulletOverLoadedException("bullet capacity is 5")
 
-    def shoot_bullet(self):
+    def shoot_bullet(self,pin):
+        self.validate_pin(pin)
         if self.is_empty():
             raise NoBulletFoundException("chamber is empty!!, add bullet")
 
@@ -29,7 +34,8 @@ class Gun:
             if self.num_of_bullet > 0:
                 self.num_of_bullet -= 1
 
-    def reload_bullet(self):
+    def reload_bullet(self,pin):
+        self.validate_pin(pin)
         if self.num_of_bullet == 0:
             self.num_of_bullet = 5
             return self.num_of_bullet
@@ -37,3 +43,9 @@ class Gun:
 
     def is_empty(self):
         return self.num_of_bullet == 0
+
+    def validate_pin(self,pin):
+        if self.pin != pin:
+            raise InvalidPinException("incorrect pin")
+        return pin
+
--- python_sweet/test_gun.py
+++ python_sweet/test_gun.py
@@ -2,68 +2,114 @@ import unittest
 
 from python_sweet.Gun import Gun
 from python_sweet.bullet_overloaded_exception import BulletOverLoadedException
+from python_sweet.invalid_pin_exception import InvalidPinException
 from python_sweet.no_bullet_found_exception import NoBulletFoundException
 
 
 class MyTestCase(unittest.TestCase):
     def test_gun_is_empty(self):
-        my_gun = Gun()
+        my_gun = Gun(1234)
         self.assertTrue(my_gun.is_empty())
 
-    def test_that_number_of_bullet_can_add_bullet(self):
-        my_gun = Gun()
-        my_gun.add_bullet()
+    def test_that_number_of_bullet_can_add_bullet_with_valid_pin(self):
+        my_gun = Gun(1234)
+        my_gun.add_bullet(1234)
         self.assertEqual(1,my_gun.get_chamber())
 
-    def test_gun_can_add_three_bullet(self):
-        my_gun = Gun()
-        my_gun.add_bullet()
-        my_gun.add_bullet()
-        my_gun.add_bullet()
+    def test_gun_can_add_three_bullet_with_valid_pin(self):
+        my_gun = Gun(1234)
+        my_gun.add_bullet(1234)
+        my_gun.add_bullet(1234)
+        my_gun.add_bullet(1234)
         self.assertEqual(3, my_gun.get_chamber())
 
-    def test_gun_can_shoot_gun(self):
-        my_gun = Gun()
-        my_gun.add_bullet()
-        my_gun.add_bullet()
-        my_gun.add_bullet()
-        my_gun.shoot_bullet()
+    def test_gun_can_shoot_gun_with_valid_pin(self):
+        my_gun = Gun(1234)
+        my_gun.add_bullet(1234)
+        my_gun.add_bullet(1234)
+        my_gun.add_bullet(1234)
+        my_gun.shoot_bullet(1234)
         self.assertEqual(2,my_gun.get_chamber())
 
     def test_gun_cannot_shoot_gun_while_there_is_no_bullet_in_the_gun(self):
-        my_gun = Gun()
+        my_gun = Gun(1234)
         with self.assertRaises(NoBulletFoundException):
-            my_gun.shoot_bullet()
+            my_gun.shoot_bullet(1234)
 
-    def test_gun_cannot_add_more_than_5_bullet(self):
-        my_gun = Gun()
+    def test_gun_cannot_add_more_than_5_bullet_with_valid_pin(self):
+        my_gun = Gun(1234)
         with self.assertRaises(BulletOverLoadedException):
-            my_gun.add_bullet()
-            my_gun.add_bullet()
-            my_gun.add_bullet()
-            my_gun.add_bullet()
-            my_gun.add_bullet()
-            my_gun.add_bullet()
+            my_gun.add_bullet(1234)
+            my_gun.add_bullet(1234)
+            my_gun.add_bullet(1234)
+            my_gun.add_bullet(1234)
+            my_gun.add_bullet(1234)
+            my_gun.add_bullet(1234)
 
-    def test_that_gun_can_reload_gun(self):
-        my_gun = Gun()
-        my_gun.add_bullet()
-        my_gun.add_bullet()
-        my_gun.shoot_bullet()
-        my_gun.shoot_bullet()
-        my_gun.reload_bullet()
+    def test_that_gun_can_reload_gun_with_valid_pin(self):
+        my_gun = Gun(1234)
+        my_gun.add_bullet(1234)
+        my_gun.add_bullet(1234)
+        my_gun.shoot_bullet(1234)
+        my_gun.shoot_bullet(1234)
+        my_gun.reload_bullet(1234)
         self.assertEqual(5,my_gun.get_chamber())
 
-    def test_that_gun_cannot_reload_more_than_the_bullet_size_raise_bullet_overloaded(self):
-        my_gun = Gun()
+    def test_that_gun_cannot_reload_more_than_the_bullet_size_raise_bullet_overloaded_with_valid_pin(self):
+        my_gun = Gun(1234)
         with self.assertRaises(BulletOverLoadedException):
-            my_gun.reload_bullet()
-            my_gun.reload_bullet()
+            my_gun.reload_bullet(1234)
+            my_gun.reload_bullet(1234)
 
-    def test_gun_can_load_gun(self):
-        my_gun = Gun()
-        my_gun.load_bullet()
+    def test_gun_can_load_gun_with_valid_pin(self):
+        my_gun = Gun(1234)
+        my_gun.load_bullet(1234)
         self.assertEqual(1,my_gun.get_chamber())
+
+    def test_gun_cannot_add_bullet_with_invalid_pin(self):
+        my_gun = Gun(1234)
+        with self.assertRaises(InvalidPinException):
+            my_gun.add_bullet(1111)
+
+    def test_gun_cannot_shoot_gun_with_invalid_pin(self):
+        my_gun = Gun(1234)
+        my_gun.add_bullet(1234)
+        with self.assertRaises(InvalidPinException):
+            my_gun.shoot_bullet(1111)
+        self.assertEqual(1,my_gun.get_chamber())
+
+    def test_gun_cannot_reload_gun_with_invalid_pin(self):
+        my_gun = Gun(1234)
+        my_gun.add_bullet(1234)
+        my_gun.shoot_bullet(1234)
+        with self.assertRaises(InvalidPinException):
+            my_gun.reload_bullet(2345)
+        self.assertEqual(0,my_gun.get_chamber())
+
+    def test_gun_cannot_load_gun_with_invalid_pin(self):
+        my_gun = Gun(1234)
+        my_gun.add_bullet(1234)
+        my_gun.add_bullet(1234)
+        my_gun.shoot_bullet(1234)
+        with self.assertRaises(InvalidPinException):
+            my_gun.load_bullet(1243)
+        self.assertEqual(1,my_gun.get_chamber())
+
+    # def test_gun_cannot_perform_any_operation_when_the_gun_is_empty_raise_exception(self):
+    #     my_gun = Gun(1234)
+    #     with self.assertRaises(BaseException):
+    #         my_gun.shoot_bullet(1234)
+    #         my_gun.load_bullet(1234)
+    #         my_gun.reload_bullet(1234)
+    #     #self.assertTrue(my_gun.is_empty())
+    #
+    def test_gun_cannot_reload_when_the_gun_is_not_empty(self):
+        my_gun = Gun(1234)
+        my_gun.add_bullet(1234)
+        with self.assertRaises(BulletOverLoadedException):
+            my_gun.reload_bullet(1234)
+
+
 
 
 
