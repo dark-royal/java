d1a0fa0ed6ae6dab9b3d35417f108ea713c00b02
dark-royal <praiseoyewole560@gmail.com>
2024-03-07 00:29:41 +0100

    python

--- logistics_services_test.py
+++ logistics_services_test.py
@@ -3,6 +3,16 @@ from unittest import TestCase
 import logistics_services
 
 
-class TestLogisticService(TestCase):
-    def test_that_collection_rate_is_less_than_50_percent(self):
-        self.assertEqual(9000, logistics_services.services(45))
+class TestLogisticsServices(TestCase):
+
+    def test_collection_rate_is_less_than_50_percent(self):
+        self.assertEqual(9000, logistics_services.services(25))
+
+    def test_collection_rate_is_between_50_to_59_percent(self):
+        self.assertEqual(16000,logistics_services.services(55))
+
+    def test_collection_rate_is_between_60_to_69_percent(self):
+        self.assertEqual(21750,logistics_services.services(67))
+
+    def test_collection_rate_is_greater_than_or_equals_to_70(self):
+        self.assertEqual(44000,logistics_services.services(78))
